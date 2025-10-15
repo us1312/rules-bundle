67e6a9ce24a1cdf0a9d7262ea360c0d8f94c1b94
RadzioWsadzio <rtadeja@initace.pl>
2025-10-15 15:52:47 +0200

    #up sv

--- src/Domain/Rule/Rule.php
+++ src/Domain/Rule/Rule.php
@@ -5,9 +5,9 @@ namespace SCA\Rules\Domain\Rule;
 final readonly class Rule {
     public function __construct(
         public string $name = 'NoName',
-        public string $event,
-        public array  $conditions,
-        public array  $actions,
+        public string $event = '',
+        public array  $conditions = [],
+        public array  $actions = [],
         public array  $parameters = [],
         public int    $weight = 0,
     ) {}
